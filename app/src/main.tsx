ed043a156d8c0a26f19a0fe3408bb4b3f7ebb3f8
Hehe-trying <tushar6420@gmail.com>
2025-06-23 20:09:53 +0530

    More Style Changes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
 import Auth from './pages/Auth'
 import Feed from './pages/Feed'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import AuthGuard from './components/AuthGuard'
-import AppLayout from './components/AppLayout'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
